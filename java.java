6410d781f0fa3b05b86cc6fafbf9505b698e8302
utkarsh <166751207+utk2602@users.noreply.github.com>
2024-11-21 22:00:43 +0530

    Create java.java

---
+++ java.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] heights = new int[n];
+        for (int i = 0; i < n; i++) {
+            heights[i] = in.nextInt();
+        }
+        int[] ans = solve(heights);
+        System.out.println(ans[0] + " " + ans[1]);
+        in.close();
+    }
+
+    public static int[] solve(int[] val) {
+        int[] index = new int[2];
+        int min = Math.abs(val[val.length - 1] - val[0]);
+        index[0] = 1;
+        index[1] = val.length;
+
+        for (int i = 0; i < val.length - 1; i++) {
+            if (Math.abs(val[i] - val[i + 1]) < min) {
+                min = Math.abs(val[i] - val[i + 1]);
+                index[0] = i + 1;
+                index[1] = i + 2;
+            }
+        }
+        return index;
+    }
+}
